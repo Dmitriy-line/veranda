1f38a02798f9ed680be2adfec44428968f1418ba
dmitriy-line <dpasternak011@gmail.com>
2021-12-16 21:05:14 +0300

    добавлено мобильное меню

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 let burger = document.querySelector('.burger');
+let nav = document.querySelector('.mobile__nav')
 
 burger.addEventListener('click', () => {
 burger.classList.toggle('active');
-});;
+nav.classList.toggle('active');
+});
+
+;
