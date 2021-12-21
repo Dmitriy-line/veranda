92723df66d0ce18c9bb9489500668e61bb12d53e
dmitriy-line <dpasternak011@gmail.com>
2021-12-21 16:29:14 +0300

    добавлена секция рестораны

--- js/script.js
+++ js/script.js
@@ -3,11 +3,13 @@ let nav = document.querySelector('.mobile__nav');
 let logo = document.querySelector('.hero-logo');
 let socials = document.querySelector('.hero-socials');
 
-burger.addEventListener('click', () => {
-burger.classList.toggle('active');
-nav.classList.toggle('active');
-logo.classList.toggle('active');
-socials.classList.toggle('active');
-});
+const openMenu = () => {
+  burger.classList.toggle('active');
+  nav.classList.toggle('active');
+  logo.classList.toggle('active');
+  socials.classList.toggle('active');
+}
+
+burger.addEventListener('click', openMenu );
 
 ;
