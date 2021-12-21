b5e6cae03d121d68193e309a8af5c2bdae80f7e2
dmitriy-line <dpasternak011@gmail.com>
2021-12-21 13:33:41 +0300

    меню

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 let burger = document.querySelector('.burger');
-let nav = document.querySelector('.mobile__nav')
+let nav = document.querySelector('.mobile__nav');
+let logo = document.querySelector('.hero-logo');
+let socials = document.querySelector('.hero-socials');
 
 burger.addEventListener('click', () => {
 burger.classList.toggle('active');
 nav.classList.toggle('active');
+logo.classList.toggle('active');
+socials.classList.toggle('active');
 });
 
 ;
